5296e73ea1840f1550f469e5c3e0d01dc1d20044
Mr. Aman <assharma2425@gmail.com>
2026-01-10 14:19:15 +0000

    datatype

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -1,8 +1,17 @@
 let score = null
 
-console.log(typeof score);
+//console.log(typeof score);
  let value = Number(score)
 
- console.log(typeof value)
- console.log(value);
+ //console.log(typeof value)
+ //console.log(value);
  
+
+let loggedIn = "22"
+
+let bool = Boolean(loggedIn)
+console.log(bool)
+
+// if any number like 1 => o/p is Ture
+// if empty string "" => o/p is False
+// if any string like "Aman" => o/p is True
