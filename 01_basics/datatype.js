c1bd0454d20cf734908b060e48e0e7d763541a9c
Mr. Aman <assharma2425@gmail.com>
2026-01-10 17:40:24 +0000

    operations

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -10,8 +10,33 @@ let score = null
 let loggedIn = "22"
 
 let bool = Boolean(loggedIn)
-console.log(bool)
+// console.log(bool)
 
 // if any number like 1 => o/p is Ture
 // if empty string "" => o/p is False
 // if any string like "Aman" => o/p is True
+
+/////////////////////////////////// OPRATIONS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+let item = 100
+
+let negValue = -item
+console.log(negValue);
+
+let str1 = "Mr."
+let str2 = "Aman"
+let str3 = str1+ str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(2+"2");
+console.log("1" + 2 + 2);
+
+console.log(1+2+"2");
+console.log(1 + 2 + "3" + 4);
+console.log(1+2+3+"4");
+
+
+
+
