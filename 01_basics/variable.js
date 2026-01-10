44a8a9a260ec8a7cdfde203f2ce54a35382ee94e
Mr. Aman <assharma2425@gmail.com>
2026-01-10 11:33:11 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const enterName = "Aman"
+let accountName = "Sbi"
+var accountId = "sharma"
+let accountPass;
+
+console.log(enterName)
+console.table([enterName, accountId , accountName , accountPass])
