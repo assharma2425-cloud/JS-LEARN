553bde991d2e98b9573f731489c9bda9bb09fd08
Mr. Aman <assharma2425@gmail.com>
2026-01-11 08:30:09 +0000

    summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,28 @@
+// Primitive
+
+// 7 datatype = String, Boolean, Null, Undefined, Number, BigInt, Symbol
+/*
+const id = Symbol(123)
+const anotherId = Symbol(123)
+
+console.log(id === anotherId);
+
+
+let score = 32456789876543n
+console.log(typeof score); */
+
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Function
+
+const arr = ["Krish", "Shaktiman", "Mogli"]
+
+let obj = {
+    name: "Aman",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
