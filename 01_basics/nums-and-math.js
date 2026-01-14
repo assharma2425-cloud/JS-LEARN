139e843f7fcae191297f16e429943d3ba3bf0ea2
Mr. Aman <assharma2425@gmail.com>
2026-01-14 10:51:07 +0000

    Number and Maths

---
+++ 01_basics/nums-and-math.js
@@ -0,0 +1,26 @@
+const score = 400
+// console.log(score);
+
+
+const hunderds = 123.8653
+// console.log(hunderds.toPrecision(4));
+
+const hunderrds = 10000000
+// console.log(hunderrds.toLocaleString('en-IN'));
+
+
+//////////////////////////////////// MTAHS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(4.7)); // 5
+// console.log(Math.ceil(5.3)); // 6
+// console.log(Math.floor(4.7)); // 4
+
+console.log(Math.random());
+console.log(Math.random()*10 +1);
+console.log(Math.floor(Math.random()*10 +1));
+
+const min  = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1))+ min);
