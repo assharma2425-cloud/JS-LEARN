999f9108c553774e0e67774c545405fb2222bc36
Mr. Aman <assharma2425@gmail.com>
2026-01-15 08:24:41 +0000

    Date & Time

---
+++ 01_basics/date&time.js
@@ -0,0 +1,18 @@
+// DATE
+
+let myDate = new Date();
+// console.log(myDate.toString()); // Output: Thu Jan 15 2026 08:08:08 GMT+0000 (Coordinated Universal Time)
+// console.log((myDate.toDateString())); // Output: Thu Jan 15 2026
+// console.log(myDate.toISOString()); // Output: 2026-01-15T08:09:45.134Z
+// console.log(myDate.toJSON()); // Output: 2026-01-15T08:09:45.134Z
+// console.log((myDate.toLocaleDateString())); // Output: 1/15/2026
+// console.log(myDate.toLocaleString()); // Output: 1/15/2026, 8:09:45 AM
+
+// console.log((typeof(myDate))); // Output: Object
+
+
+// let myCreateDate = new Date(2026, 6, 21)
+let myCreateDate = new Date(2026, 6, 21, 05, 30)
+
+console.log(myCreateDate.toString());
+console.log(myCreateDate.toLocaleString());
