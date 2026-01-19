8fd07810bbb22fe2d9ac803ce4978d804e1b4aeb
Mr. Aman <assharma2425@gmail.com>
2026-01-19 12:35:26 +0000

    Objects

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,3 +1,25 @@
-const item = new Object(undefined);
-item.foo = 42;
-console.log(item);
+// const item = new Object(undefined);
+// item.foo = 42;
+// console.log(item);
+const mySym = Symbol("key1")
+const myObj = {
+    name : "Aman",
+    "full name": "Aman Sharma",
+    [mySym] : "mykey1",
+    email : "as@google.com"
+}
+
+// console.log(myObj["name"]);
+// console.log(myObj["full name"]);
+// console.log(typeof myObj[mySym]);
+
+myObj.email = "ass@microsoft.com"
+// Object.freeze(myObj)
+myObj.email = "assss.com"
+// console.log(myObj);
+
+myObj.gretting = function(){
+    console.log("hello JS user");
+}
+console.log(myObj.gretting());
+
