49ea668cb194ea124bafdf11938ac267199b5635
Mr. Aman <assharma2425@gmail.com>
2026-01-21 05:44:37 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,21 @@
+function myName(){
+    console.log("A");
+    console.log("m");
+    console.log("A");
+    console.log("n");
+}
+// myName()
+
+// function sumNumber(number1 , number2){
+//     console.log(number1 + number2);
+// }
+
+function sumNumber(number1 , number2){
+    // let result = (number1 + number2)
+    // return result
+    return number1 + number2
+}
+const result = sumNumber(44,55)
+// console.log(result);
+
+
