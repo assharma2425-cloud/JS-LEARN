3f252447e9351e457ec7c42a0a318f4bd7c8e3ee
Mr. Aman <assharma2425@gmail.com>
2026-01-21 07:18:33 +0000

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,3 +19,42 @@ const result = sumNumber(44,55)
 // console.log(result);
 
 
+function loginUser(username="aman"){
+    if(!username){
+        console.log("Please enter username");
+        return
+    }
+    else{
+    return `${username} you logged in`
+    }
+}
+// console.log(loginUser(""))
+
+
+// function
+
+function calculateUser(...num1){
+    return num1                                     // its a rest operator or spred operator
+}
+// console.log(calculateUser(200, 300, 400));
+
+
+const users = {
+    username : "Aman",
+    price : 199
+}
+
+function userLogin(anyobject){
+    return `username is ${anyobject.username} and the price is ${anyobject.price}`
+}
+// console.log(userLogin(users));
+
+
+// array
+
+const newArr = [200, 300, 500, 400]
+
+function userArr(anyArr){
+    return anyArr[2]
+}
+console.log(userArr(newArr))
